10cd7bfd635a14914b44ad2845124d55294f520d
NPCFISH <93332534+TheNPCFISH@users.noreply.github.com>
2022-05-26 14:56:05 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='SnowForPython',
+    version='0.1',    
+    description='A package for using Snow datastores in python.',
+    url='https://github.com/TheNPCFISH/SnowForPython',
+    author='NPCFISH',
+    author_email='',
+    license='Apache License 2.0',
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows :: Windows 10',        
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
